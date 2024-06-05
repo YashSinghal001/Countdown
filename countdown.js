625430ac276a1fb8c910e4f4eebbd32654e0c18c
Yash Singhal <csdsa772023@gmail.com>
2024-06-05 16:49:56 +0530

    Add files via upload
    Countdown Through JS

---
+++ countdown.js
@@ -0,0 +1,30 @@
+let dayItem=document.querySelector("#days");
+let hoursItem=document.querySelector("#hours");
+let minItem=document.querySelector("#min");
+let secItem=document.querySelector("#sec");
+
+let countDown = () =>{
+    let futureDate= new Date("10 jun 2024 2:00 PM");//Js Date object show time in millisecond if we use getTimeout() from 1 Januray 1970 till  to 10 june 2024.Otherwise It show date and time of that day( 10 jun 2024)
+    let currentDate=new Date();//Js Date object show time in millisecond if we use getTimeout()  from 1 Januray 1970 till  to Now june 2024.Otherwise It show date and time  of Now Day 2024
+    // console.log(futureDate);
+    // console.log(currentDate);
+    let launchDate=futureDate - currentDate;
+    // console.log(launchDate);//It show differnce from Now Date to Future Date in Milliseconds
+
+    let days=Math.floor(launchDate / 1000 / 60 /60 / 24);//convert second then minutes then hours then seconds then finally day
+    let hours=Math.floor(launchDate / 1000/60/60)%24;//convert second then minutes then hours then modolus with 24 for remaining hours
+    let min=Math.floor(launchDate / 1000/60)%60;//convert second then minutes then modolus with 60 for remaining minutes
+    let sec=Math.floor(launchDate / 1000)%60;//convert second  then modolus with 60 for remaining seconds
+
+    dayItem.innerHTML=days;
+    hoursItem.innerHTML=hours;
+    minItem.innerHTML=min;
+    secItem.innerHTML=sec;
+
+}
+// Call
+countDown()
+// Refersh Each 1 Seconds
+    setInterval(()=>{
+        countDown()
+    },1000);
